e9442755e5fd2e1331a2f243eac74db9281f7709
MariamAshraf200 <mariamsarhan201@gmail.com>
2024-09-24 06:57:43 +0300

    ..

---
+++ lib/features/user/data/reposatories/user_repo_impl.dart
@@ -0,0 +1,85 @@
+import 'package:check_weather/core/errors/exceptions.dart';
+import 'package:check_weather/core/errors/failures.dart';
+import 'package:check_weather/core/network/networkInfo.dart';
+import 'package:check_weather/features/user/data/dataScorces/remoteDataScorce.dart';
+import 'package:check_weather/features/user/domain/entities/user_entity.dart';
+import 'package:check_weather/features/user/domain/repositories/user_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class UserRepoImpl implements UserRepo{
+  final RemoteDataScorce remoteDataScorce;
+  final Networkinfo networkinfo;
+
+  UserRepoImpl({required this.remoteDataScorce, required this.networkinfo});
+
+  @override
+  Future<Either<Failures, UserEntity>> login(String email, String password) async {
+    if(await networkinfo.isConnected){
+      try {
+        final usermodel = await remoteDataScorce.login(email, password);
+        UserEntity userEntity = UserEntity(id: usermodel.id,
+            name: usermodel.name,
+            email: email,
+            phone: usermodel.phone);
+        return Right(userEntity);
+      }on ServerException{
+        throw ServerFailuer();
+      }on WrongDataException{
+        throw WrongDataFailuer();
+      }
+    }else{
+      throw OfflineFailuer();
+    }
+  }
+
+  @override
+  Future<Either<Failures, Unit>> logout() async {
+    if(await networkinfo.isConnected){
+      try {
+      await  remoteDataScorce.logout();
+        return Right(unit);
+      }on ServerException{
+        throw ServerFailuer();
+      }
+    }else{
+      throw OfflineFailuer();
+    }
+  }
+
+  @override
+  Future<Either<Failures, Unit>> ressetpassword(String email) async {
+    if(await networkinfo.isConnected){
+      try {
+        await  remoteDataScorce.ressetPassword(email);
+        return Right(unit);
+      }on ServerException{
+        throw ServerFailuer();
+      }
+    }else{
+      throw OfflineFailuer();
+    }
+  }
+
+  @override
+  Future<Either<Failures, UserEntity>> signup(String email, String name, String phone, String password) async {
+    if(await networkinfo.isConnected){
+      try {
+        final usermodel = await remoteDataScorce.signup(email, phone, name, password);
+
+        UserEntity userEntity = UserEntity(id: usermodel.id,
+            name: usermodel.name,
+            email: email,
+            phone: usermodel.phone);
+        return Right(userEntity);
+      }on ServerException{
+        throw ServerFailuer();
+      }on WrongDataException{
+        throw WrongDataFailuer();
+      }
+    }else{
+      throw OfflineFailuer();
+    }
+
+  }
+
+}
---
+++ lib/features/user/domain/usecases/user_login.dart
@@ -0,0 +1,15 @@
+import 'package:check_weather/core/errors/failures.dart';
+import 'package:check_weather/features/user/domain/entities/user_entity.dart';
+import 'package:check_weather/features/user/domain/repositories/user_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class UserLoginUsecase{
+  final UserRepo repo;
+
+  UserLoginUsecase({required this.repo});
+
+
+  Future<Either<Failures, UserEntity>> call(String email,String password) async{
+    return await repo.login(email, password);
+  }
+}
